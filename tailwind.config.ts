e9d09f042258926cb28f6301bd02f42a2337d29d
Tiago Prenhaca <prenhacatiago@gmail.com>
2023-10-04 23:20:27 -0300

    feat: adição de imagens do hero, menu, antd e hero do mobile

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,13 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        yellow: '#CA991A',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
